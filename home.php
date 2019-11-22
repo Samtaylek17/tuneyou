94c2518492efbfa5277a7d8c7b1e2028ed496f66
Samtaylek17 <samtaylek@gmail.com>
2019-11-22 11:56:16 +0100

    Tuneyou on the way

---
+++ home.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tuneyou</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="assets/css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="assets/css/home.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-fluid" style="padding:0px;">
+        <div class="content-header">
+            <header>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4 col-xs-6">
+                            <a href="#">
+                                <h1 class=" text-white">Tuneyou</h1>
+                            </a>
+                        </div>
+                        <div class="col-md-8 text-right">
+                            <ul class="nav text-white pull-right">
+                                <li class="pt-1">
+                                    <a href="signIn.php" class="font-weight-bold">SIGN IN</a>
+                                </li>
+                                <li>
+                                    <a href="signUp.php"><button class="get-started">GET STARTED</button></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <div class="header-text">
+                <h1 class=" text-white">You make the <br>Tune. We make <br>it GO.</h1>
+                <p class="p-small">Securely store your creative work, protect <br>your rights, distribute your music and
+                    collect <br>your royalties worldwide with Tuneyou.
+                </p>
+                <a href="signUp.php"><button>GET STARTED FREE</button></a>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="container mt-5">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="non-artist text-center mt-5">Not An Artist? <br> No Problem</h2>
+                        <p class="text-center p-small">
+                            TuneYou is the biggest singing competition that
+                            offers the <br>best platform for all aspiring singers
+                            to showcase their talent. <br>Bring yourself to the 
+                            international stage and share your talent.
+                        </p>
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <h2 class="non-artist text-center">
+                            WATCH PAST PERFORMANCES
+                        </h2>
+                        <iframe width="100%" height="250" src="https://www.youtube.com/embed/Wo5uf4-WCdE" 
+                        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen></iframe>
+                        <a href="index.php"><button class="watch-all"><i class="fas fa-video"></i> Watch All Performance</button></a>
+                    </div>
+                </div>
+                <div class="mt-5 text-center">
+                    <h2 class="non-artist text-center">TuneYou Gallery</h2>
+                    <p class="p-small">Check out our competition past winners and other performances</p>
+                    <div class="row">
+                        <div class="col-md-3 pr-0">
+                            <img src="assets/images/guitarist.jpg" class="img-grid">
+                        </div>
+                        <div class="col-md-3 pr-0">
+                            <img src="assets/images/musician.jpg" class="img-grid">
+                        </div>
+                        <div class="col-md-3 pr-0">
+                            <img src="assets/images/singer.jpg" class="img-grid">
+                        </div>
+                        <div class="col-md-3 pr-0">
+                            <img src="assets/images/MusicCompetition_05.jpg" class="img-grid">
+                        </div>
+                    </div>
+                    <div class="row mt-3 mb-5">
+                        <div class="col-md-3 pr-0">
+                            <img src="assets/images/guitarist.jpg" class="img-grid">
+                        </div>
+                        <div class="col-md-3 pr-0">
+                            <img src="assets/images/musician.jpg" class="img-grid">
+                        </div>
+                        <div class="col-md-3 pr-0">
+                            <img src="assets/images/singer.jpg" class="img-grid">
+                        </div>
+                        <div class="col-md-3 pr-0">
+                            <img src="assets/images/MusicCompetition_05.jpg" class="img-grid">
+                        </div>
+                    </div>    
+                    <button class="get-started-now">VIEW MORE</button>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-6">
+                        <img src="assets/images/singer.jpg" class="img-responsive">
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <h2 class="non-artist text-center mt-5">
+                            What do you win? <br> A Chance to shine!
+                        </h2>
+                        <p class="p-small">
+                            A prize is awarded to the Top 5 finishers in our contest.<br>
+                            There is one Grand Prize winner and four runner-up prizes.
+                        </p>
+                    </div>
+                </div>
+                <div class="mt-5 text-center">
+                    <h2 class="non-artist">Become a Star</h2>
+                    <p class="p-small">Submit your contest entry or register as a fan to vote
+                        your favourite contest entry
+                    </p>
+                    <a href="signUp.php"><button class="get-started-now">Get Started</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="bg-dark mt-5 py-5">
+        <div class="container text-white">
+            <div class="row justify-content-center">
+                <a href="privacy-policy.html">Privacy Policy</a>&nbsp;.&nbsp;
+                <a href="terms.html">Terms of Service</a>&nbsp;.&nbsp;
+                <a href="rules.html">Contest Rules</a>&nbsp;.&nbsp;
+                <a href="contact.html">Contact Us</a>
+            </div>
+            <div class="row justify-content-center">
+                <p class="text-center">&copy; 2019 TuneYou. All rights reserved. TuneYou
+                    takes copyright infringement very seriously, and abides by 
+                    the Digital Millennium Copyright Act. Fore more information, 
+                    please carefully review our Terms of Service
+                </p>
+            </div>
+        </div>
+    </footer>
+    <!-- SCRIPTS -->
+<!-- JQuery -->
+<script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="assets/js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="assets/js/mdb.min.js"></script>
+</body>
+</html>
